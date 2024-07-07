9d480d39b29b4ba034e8af79732cbe848e4a823e
Llamuca Dueñas Sebastián <llamucasebas@gmail.com>
2024-07-07 18:17:13 -0500

    chore: update tailwind.config.mjs with container settings

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,6 +7,16 @@ export default {
         monserrat: ["Montserrat", "sans-serif"],
         firaSans: ["Fira Sans", "sans-serif"],
       },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          sm: "2rem",
+          lg: "4rem",
+          xl: "5rem",
+          "2xl": "6rem",
+        },
+      },
     },
   },
   plugins: [],
